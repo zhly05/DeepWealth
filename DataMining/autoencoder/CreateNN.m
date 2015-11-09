691780a385c1c796ee4da193591db9ffd82c756e
Lingyu Zhang <lz2365@columbia.edu>
2015-11-09 17:25:20 +0800

    Recoding NeuronNetwork#1
    Finished 'CreateNN.m'. Set the parameters of NN.

--- DataMining/autoencoder/CreateNN.m
+++ DataMining/autoencoder/CreateNN.m
@@ -1,17 +1,35 @@
-function [ NN ] = CreateNN( NN_size )
-%CREATENN 此处显示有关此函数的摘要
-%   此处显示详细说明
+function [ NN ] = CreateNN( NN_size , NN_type)
+% Create a standard Neuron Network with several layers.
+% size(1) indicts the number of input. size(end) is for the output layer.
+% There are length(size)-2 hidden layers.
+% NN.HiddenLayers are struct array with NNsize-1 layers in it.
+% Input --> HiddenLayers(1) --> ... --> HiddenLayers(layersN-1) --> Output
+%
+% Type
+%       f: Fully connected;
+%       c: Convolutional;
+%       s: Subsample;
+
     NN.size = NN_size;
-    NN.layersN = length(NN_size);
+    NN.layersN = length(NN_size) - 1;
+    if nargin == 1
+        NN_type = cellstr(repmat('f', NN.layersN, 1))';
+    elseif nargin == 2
+        assert(length(NN_type) == NN.layersN, 'need length(size)-1 letters to format the type of NN');
+        if ischar(NN_type)
+            NN_type = cellstr(reshape(NN_type, NN.layersN, 1))';
+        elseif iscell(NN_type)
+            NN_type = reshape(NN_type, NN.layersN, 1)';
+        else
+            error('Create NN Failed');
+        end
+    end
+    NN.HiddenLayers = struct('type',lower(NN_type));
     
     NN.iterN = 1000;
-    
     NN.alpha = 0.005;
     NN.beta = 0;0.001;
     NN.lamda = 0;0.001;
     NN.rho = 0.05;
-    
-    
-    
 end
 
